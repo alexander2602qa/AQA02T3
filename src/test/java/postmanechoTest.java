65878d919b12bfed7b2b8fee9bfaea548c113c67
Alexander <alex.albasov@gmail.com>
2023-06-24 15:51:39 +0700

    task with fall test

--- src/test/java/postmanechoTest.java
+++ src/test/java/postmanechoTest.java
@@ -14,7 +14,7 @@ public class postmanechoTest {
             .post("/post")
         .then()
             .statusCode(200)
-            .body("data", equalTo("some date"));
+            .body("data", equalTo("somedate"));
     }
 
 }
