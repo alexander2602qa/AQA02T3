79da2dc6895cf6b7149e328f23d02574cf41cce5
Alexander <alex.albasov@gmail.com>
2023-06-24 15:54:18 +0700

    task with passed test

--- src/test/java/postmanechoTest.java
+++ src/test/java/postmanechoTest.java
@@ -14,7 +14,7 @@ public class postmanechoTest {
             .post("/post")
         .then()
             .statusCode(200)
-            .body("data", equalTo("somedate"));
+            .body("data", equalTo("some date"));
     }
 
 }
